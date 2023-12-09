3907226fc8a2468af4540b7cf1071d3425cb8754
Carlo Santos <rcarlosantos89@gmail.com>
2023-12-09 10:30:02 -0600

    first draft from right

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ document.getElementById('navToggle').addEventListener('click', function () {
   document.getElementById('dropdown').classList.toggle('active');
 });
 
+
+document.getElementById("about-link").addEventListener('click', function () {
+  document.getElementById('dropdown').classList.toggle('active');
+  document.getElementById('about').classList.add('active');
+  document.getElementById('hero').classList.add('inactive');
+})
